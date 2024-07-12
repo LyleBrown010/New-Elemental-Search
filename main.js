a934bce5dd56f22b4a85e354dd59c6e36b3a83a5
LyleBrown010 <lylebrown010@gmail.com>
2024-07-12 08:14:04 +0200

    done

--- main.js
+++ main.js
@@ -8,65 +8,65 @@ $(document).ready(function(){
     })
 
     $("#searchMain").on("input", function(){
-        let mainSearch = $(this).val().toLowerCase();
-        let searchResult = elementalNames.filter(item => item.name.toLowerCase().includes(mainSearch))
+        let mainSearch = $(this).val().toLowerCase(); // take value entered, turns to lowercase
+        let searchResult = elementalNames.filter(item => item.name.toLowerCase().includes(mainSearch)) // filters through JSON "name" turns it lowercase and checks if it contains the val entered in search. 
 
-        showResults(searchResult); 
+        showResults(searchResult); // calls function
     })
 
     function showResults(results){
-        let resultsList = $("#searchResults"); 
-        resultsList.empty();
+        let resultsList = $("#searchResults"); // targets results section, serves as the big section where results are displayed
+        resultsList.empty(); // clears whatever data is stored in the "list" before executing function
 
-        if(results.length > 0 ){
-            results.forEach(item => {
-                resultsList.append(`<li>${item.name}</li>`);
+        if(results.length > 0 ){ //not empty
+            results.forEach(item => { //loops through each item in the array
+                resultsList.append(`<li>${item.name}</li>`); //appends item result  
             })
-            resultsList.show();
+            resultsList.show(); //shows results
         }
         else{
-            resultsList.hide()
+            resultsList.hide() // basically display none
         }
     }
     // keyboard nav 
     $("#searchMain").on("keydown", function(keyboard){
-        let results = $("#searchResults li");
-        let selectedResult = results.filter(".selectedResult");
+        let results = $("#searchResults li"); //gets lists items from search
+        let selectedResult = results.filter(".selectedResult"); //filters through results to find the class 
 
         if(keyboard.key === "ArrowDown"){
-            keyboard.preventDefault();
-            if(selectedResult.length === 0){
-                results.first().addClass("selectedResult")
+            keyboard.preventDefault(); // prevent scrolling
+            if(selectedResult.length === 0){ 
+                results.first().addClass("selectedResult") //no item selected takes first item and adds a class
             }
             else if (selectedResult.next().length > 0){
-                selectedResult.removeClass("selectedResult").next().addClass("selectedResult")
+                selectedResult.removeClass("selectedResult").next().addClass("selectedResult") // removes class from first selected item and adds it to the next
             }
         }
         else if(keyboard.key === "ArrowUp"){
-            keyboard.preventDefault();
+            keyboard.preventDefault(); //prevent scrolling
             if(selectedResult.length === 0){
-                results.last().addClass("selectedResult")
+                results.last().addClass("selectedResult") //none selected, takes first item
             }
             else if (selectedResult.prev().length > 0){
-                selectedResult.removeClass("selectedResult").prev().addClass("selectedResult")
+                selectedResult.removeClass("selectedResult").prev().addClass("selectedResult") //something selected, removes class from first selected, adds class to previous item
             }
         }
-        else if(keyboard.key === "Enter"){
+        else if(keyboard.key === "Enter"){ //enter key is pressed
             if(selectedResult.length > 0){
-                $(this).val(selectedResult.text());
-                $("#searchResults").hide();
+                $(this).val(selectedResult.text()); // whatever li its on gets added to search bar, through the given class
+                $("#searchResults").hide(); // hides the results, basically a display none
             }
         }
     });
 
     // click function
-    $(document).on("click", "#searchResults li", function() {
-        $("#searchMain").val($(this).text());
+    $(document).on("click", "#searchResults li", function() { //clicked on list item, runs similar to enter key
+        $("#searchMain").val($(this).text()); //targets search, adds selected value to it
         $("#searchResults").hide();
     });
 
     // close results when clicking outside the results tab
-    $(document).on("click", function(close){
+    $(document).on("click", function(close){ //attach to doc so that function executes on whole doc, not one section
         if(!$(close.target).closest("#searchMain", "#searchResults").length){
             $("#searchResults").hide();
         }
